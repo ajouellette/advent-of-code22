a828e22bdeb4e483365409445e9fefcf8e80d053
Aaron Ouellette <aaronjo2@illinois.edu>
2022-12-08 10:43:27 -0600

    allow non-numeric answers

--- template.py
+++ template.py
@@ -13,14 +13,14 @@ def main():
     test = "test" in sys.argv
     if test:
         with open("input_test") as f:
-            answers = list(map(int, f.readline().split()))
+            answers = f.readline().split()
             input_txt = f.read()
     else:
         with open("input") as f:
             input_txt = f.read()
 
     if test:
-        print("Correct answers:", answers)
+        print("Correct answers:", *answers)
 
     print(part1(input_txt))
     print(part2(input_txt))
